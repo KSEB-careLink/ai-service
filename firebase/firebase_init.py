563a0f14e12f67dc94004691ac369beba1681bfa
김경민 <168668632+enterinthere@users.noreply.github.com>
2025-07-07 19:33:40 +0900

    Add files via upload

---
+++ firebase/firebase_init.py
@@ -0,0 +1,17 @@
+# firebase/firebase_init.py
+import os
+import firebase_admin
+from firebase_admin import credentials, storage
+
+if not firebase_admin._apps:
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    json_path = os.path.join(base_dir, "serviceAccountKey.json")
+    cred = credentials.Certificate(json_path)
+
+    # ✅ 먼저 초기화
+    firebase_admin.initialize_app(cred, {
+        "storageBucket": "carelink-a228a.firebasestorage.app"
+    })
+
+# ✅ 초기화 이후에 bucket 선언
+bucket = storage.bucket()
