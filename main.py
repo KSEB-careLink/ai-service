28bb6be0a6ae85d70001feca05bb65db0cfdee45
김경민 <168668632+enterinthere@users.noreply.github.com>
2025-08-10 15:53:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from firebase.firebase_init import bucket
 from scripts.register_voice import router as voice_router
 from routes.reminder import router as reminder_router
 from routes.quiz import router as quiz_router
+from routes.predict_accuracy import router as predict_router 
 
 app = FastAPI()
 
@@ -25,7 +26,9 @@ bucket = bucket
 app.include_router(voice_router)
 app.include_router(reminder_router)
 app.include_router(quiz_router)
+app.include_router(predict_router)
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
+
